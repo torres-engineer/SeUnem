421274338e4ce5dd8560b2c0e2009ca3a9b0bcc2
João Augusto Costa Branco Marado Torres <torres.dev@disroot.org>
2025-10-15 21:50:43 +0100

    feat: eslint
    Signed-off-by: João Augusto Costa Branco Marado Torres <torres.dev@disroot.org>

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 pnpm i && pnpm build
 composer run dev
 ```
+
+- https://laravel.com/docs/12.x/lifecycle
+- https://inertiajs.com/pages
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-	plugins: [
-		laravel({
-			input: ['resources/css/app.css', 'resources/js/app.js'],
-			ssr: 'resources/js/ssr.js',
-			refresh: true
-		}),
-		tailwindcss(),
-		svelte()
-	],
-	resolve: (name) => {
-		const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true });
+  plugins: [
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.js"],
+      ssr: "resources/js/ssr.js",
+      refresh: true,
+    }),
+    tailwindcss(),
+    svelte(),
+  ],
+  resolve: (name) => {
+    const pages = import.meta.glob("./Pages/**/*.svelte", { eager: true });
 
-		return pages[`./Pages/${name}.svelte`];
-	}
+    return pages[`./Pages/${name}.svelte`];
+  },
 });
